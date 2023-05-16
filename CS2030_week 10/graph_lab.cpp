dd424ea926e5256f80583ae3830d248da8dd839d
Nusrath-Amana <129715690+Nusrath-Amana@users.noreply.github.com>
2023-05-16 16:57:27 +0530

    Add files via upload

---
+++ CS2030_week 10/graph_lab.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+struct Node {
+    int label;
+    list<int> neighbours;
+};
+
+struct Graph {
+    int n = 9; // Increase the size of the array to accommodate node 8
+    Node* nodes = new Node[n];
+
+    void initializenodes() {
+        for (int i = 1; i < n; i++) {
+            nodes[i].label = i;
+        }
+    }
+
+    void addedge(int u, int v) {
+        nodes[u].neighbours.push_back(v);
+        nodes[v].neighbours.push_back(u);
+    }
+
+    void print() {
+        for (int i = 1; i < n; i++) {
+            cout << i << " --> ";
+            for (int neighbour : nodes[i].neighbours) {
+                cout << neighbour << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Graph* g = new Graph;
+    g->initializenodes();
+
+    // Add edges for the graph
+    g->addedge(1, 2);
+    g->addedge(1, 3);
+    g->addedge(1, 4);
+    g->addedge(1, 5);
+    g->addedge(2, 3);
+    g->addedge(2, 6);
+    g->addedge(4, 6);
+    g->addedge(4, 7);
+    g->addedge(4, 8);
+    g->addedge(5, 6);
+    g->addedge(5, 7);
+    g->addedge(5, 8);
+
+    // Print the graph adjacency list
+    g->print();
+
+}
+
+
